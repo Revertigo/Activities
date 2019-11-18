a5d1e341cae25c807105191e6326a257a8ccd600
orh92 <orh92@walla.co.il>
2019-11-18 21:07:43 +0200

    Add logout button+the funtions

--- app/src/main/java/com/example/activities/SearchPost.java
+++ app/src/main/java/com/example/activities/SearchPost.java
@@ -2,13 +2,31 @@ package com.example.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SearchPost extends AppCompatActivity {
-
+  private Button buttonLogout;
+   FirebaseAuth auth;
+  private FirebaseAuth.AuthStateListener authListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_post);
+
+        buttonLogout=findViewById(R.id.button7);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.getInstance().signOut();
+                Intent intent =new Intent(SearchPost.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
