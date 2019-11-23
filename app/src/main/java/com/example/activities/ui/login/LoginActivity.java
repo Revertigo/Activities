cdd6984b1f3f1b3385ab5b066da91819020d0ca3
DESKTOP-JMEM6VH\Dekel <dekel241@gmail.com>
2019-11-23 20:13:05 +0200

    Remove unnecessary imports from login activity

--- app/src/main/java/com/example/activities/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/activities/ui/login/LoginActivity.java
@@ -1,40 +1,30 @@
 package com.example.activities.ui.login;
 
-import android.app.Activity;
-
 import androidx.annotation.NonNull;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.activities.MainActivity;
 import com.example.activities.R;
 import com.example.activities.RegisterToApp;
 import com.example.activities.SearchPost;
-import com.example.activities.ui.login.LoginViewModel;
-import com.example.activities.ui.login.LoginViewModelFactory;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 public class LoginActivity extends AppCompatActivity {
 
