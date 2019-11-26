2ad058ef491304b28897635a4c5104801304f754
orh92 <orh92@walla.co.il>
2019-11-26 13:02:41 +0200

    Add items to Spinners(location,activity,level), Add Search button,Add Post button

--- app/src/main/java/com/example/activities/SearchPost.java
+++ app/src/main/java/com/example/activities/SearchPost.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,6 +19,37 @@ public class SearchPost extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_post);
+
+
+        String[] arraySpinner1 = new String[] {
+                "Enter Location","North", "Middle", "South","Another"
+        };
+        String[] arraySpinner2 = new String[] {
+                "Enter Activity","Sport", "Food", "Fun","Another"
+        };
+        String[] arraySpinner3 = new String[] {
+                "Enter Level","Beginner", "Advanced", "Expert"
+        };
+
+        Spinner spin1 = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> adapterForSpinner2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner2);
+        adapterForSpinner2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin1.setAdapter(adapterForSpinner2);
+
+        Spinner spin2 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapterForSpinner1 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner1);
+        adapterForSpinner1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin2.setAdapter(adapterForSpinner1);
+
+
+        Spinner spin3 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapterForSpinner3 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner3);
+        adapterForSpinner3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin3.setAdapter(adapterForSpinner3);
+
 
         buttonLogout=findViewById(R.id.button7);
         buttonLogout.setOnClickListener(new View.OnClickListener() {
