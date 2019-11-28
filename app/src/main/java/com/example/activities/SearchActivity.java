bfa54fc87bc7ffb25955b9c68373c3739c7d8701
orh92 <orh92@walla.co.il>
2019-11-29 00:09:25 +0200

    Add close app button, Add log out button, Add search bar,Add search button

--- app/src/main/java/com/example/activities/SearchActivity.java
+++ app/src/main/java/com/example/activities/SearchActivity.java
@@ -2,13 +2,47 @@ package com.example.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private Button buttonLogout;
+    private FirebaseAuth auth;
+    private Button closeAppFromSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+
+        //close app button
+        closeAppFromSearch=findViewById(R.id.button18);
+        closeAppFromSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                moveTaskToBack(true);
+
+            }
+        });
+
+        //log out button
+        buttonLogout=findViewById(R.id.button19);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.getInstance().signOut();
+                Intent intent =new Intent(SearchActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
