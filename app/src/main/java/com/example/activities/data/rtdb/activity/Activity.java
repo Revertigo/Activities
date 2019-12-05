b7b33b18a4371a37c80930a500cc936da7fb5b17
orh92 <orh92@walla.co.il>
2019-12-05 17:12:10 +0200

    Add getters setters -Add array with the data in strings -the function returns the array of strings

--- app/src/main/java/com/example/activities/data/rtdb/activity/Activity.java
+++ app/src/main/java/com/example/activities/data/rtdb/activity/Activity.java
@@ -16,6 +16,8 @@ public class Activity {
     private Date date;
     private String time;//Format: hh:mm
 
+    String data_array[]=new String[11];//array with the strings of the data(easy insert to database)
+
     private static class Address
     {
         private String city_set;//City or settlement
@@ -27,12 +29,77 @@ public class Activity {
             this.street = street;
             this.apartment_number = apartment_number;
         }
+        public String getCity(){ return this.city_set;}
+        public String getStreet(){return this.street;}
+        public String getApartNumString(){return Integer.toString(this.apartment_number);}
+        public int getApartNumInt(){return this.apartment_number;}
     }
+public String[] getData(){
+       data_array[0]=Float.toString(this.id); // the id of the activity
+       data_array[1]=this.name;//the name of the activity
+       data_array[2]=this.Type;//type of activity
+       data_array[3]=this.addr.getCity();//city of the activity
+       data_array[4]=this.addr.getStreet();//street of the activity
+       data_array[5]=this.addr.getApartNumString();//apartment number of the activity
+       data_array[6]=this.difficulty;//the difficult of the activity
 
+       if(single_group==true){
+       data_array[7]="true";}//group
+       else{data_array[7]="false";} //singe
+       if(gender.equals(0)){//gender of the activity
+       data_array[8]="female";}
+       else if(gender.equals(1)){data_array[8]="male";}//gender of the activity
+       else{data_array[8]="both";} //gender of the activity
+       data_array[9]=this.description; //the describe of the activity
+       data_array[10]="date"; //the date of the activity post
+       data_array[11]=this.time;//format
+       return this.data_array;
+    }
     private enum Gender
     {
         FEMALE, MALE, BOTH
     }
 
+    //id
+    public long getID(){return this.id;}
+    public void setID(long id){this.id=id;}
+
+   //name
+   public String getName(){return this.name;}
+    public void setName(String name){this.name=name;}
+
+    //type
+    public String getType(){return this.Type;}
+    public void setType(String type){this.Type=type;}
+
+    //addr
+    public Address getAddr(){return this.addr;}
+
+    //difficulty
+
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    public String getDifficulty(){return this.difficulty;}
+
+    //activity for single or group getter setter
+    public void setSingle_Group(boolean ans){this.single_group=ans;}
+    public boolean getSingle_Group(){return this.single_group;}
+
+    //gender of the activity
+    public void setGender(Gender gender){this.gender=gender;}
+    public Gender getGender(){return this.gender;}
+
+    //description of the activity
+    public void setDescription(String description){this.description=description;}
+    public String getDescription(){return this.description;}
+
+    //date
+    public Date getDate(){return this.date;}
+
+    //time format
+    public String getTime(){return this.time;}
+    public void setTime(String time){this.time=time;}
 
 }
