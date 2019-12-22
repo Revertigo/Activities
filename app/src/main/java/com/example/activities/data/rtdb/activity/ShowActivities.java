a68c350cf8979615a52c21c58bb12b55ff93e108
orh92 <orh92@walla.co.il>
2019-12-22 21:24:27 +0200

    Add buttons on ShowActivities
    -Back to search or post activity
    -Back to main search activity

--- app/src/main/java/com/example/activities/data/rtdb/activity/ShowActivities.java
+++ app/src/main/java/com/example/activities/data/rtdb/activity/ShowActivities.java
@@ -5,19 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.example.activities.MainActivity;
 import com.example.activities.R;
+import com.example.activities.SearchActivity.SearchActivity;
+import com.example.activities.SearchActivity.SearchOrPost;
 
 import java.util.ArrayList;
 
 public class ShowActivities extends AppCompatActivity {
+    private Button searchOrPost;
+    private Button searchMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_activities);
+
+        searchOrPost=findViewById(R.id.backToMainActivity);
+        searchMenu=findViewById(R.id.backToSearchActivity);
+        searchOrPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ShowActivities.this, SearchOrPost.class);
+                startActivity(intent);
+            }
+        });
+
+        searchMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(ShowActivities.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
 
         ArrayList<Activity> theActivities;
         theActivities=getIntent().getParcelableArrayListExtra("activitiesArray");
@@ -25,6 +51,7 @@ public class ShowActivities extends AppCompatActivity {
         ListView activityListView=findViewById(R.id.listViewShowActivities);
         ArrayAdapter<Activity> adapter =new ArrayAdapter<Activity>(ShowActivities.this,android.R.layout.simple_list_item_1,theActivities);
         activityListView.setAdapter(adapter);
+        //Todo need to fix the 0 1 10 id sorting by the adapter or listvew
 
     }
 }
