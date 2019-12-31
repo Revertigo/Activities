bca007736b63bfdaaa5baed57de8452015d493f7
orh92 <orh92@walla.co.il>
2019-12-31 20:50:29 +0200

    Edit user Profile Design

--- app/src/main/java/com/example/activities/data/rtdb/activity/UserProfile.java
+++ app/src/main/java/com/example/activities/data/rtdb/activity/UserProfile.java
@@ -5,12 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.activities.R;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +17,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserProfile extends AppCompatActivity {
     private TextView name,email,occupation,education,gender,permission,birthday;
-    private ImageView emailImage,occupationImage,educationImage,genderImage,permissionImage,birthdayImage,profileImage;
+   // private ImageView emailImage,occupationImage,educationImage,genderImage,permissionImage,birthdayImage,profileImage;
     private FirebaseDatabase database;
     private DatabaseReference userRef;
 
@@ -37,18 +35,18 @@ public class UserProfile extends AppCompatActivity {
         name=findViewById(R.id.nameOfTheUser);
         email=findViewById(R.id.emailOfTheUser);
         occupation=findViewById(R.id.occupationOfTheUser);
-        education=findViewById(R.id.educationOfTheUser);
+        education=findViewById(R.id.educationOfTheUserTitle);
         gender=findViewById(R.id.genderOfTheUser);
         permission=findViewById(R.id.permissionOfTheUser);
         birthday=findViewById(R.id.birthdayOfTheUser);
-
-        emailImage=findViewById(R.id.emailImage);
-        occupationImage=findViewById(R.id.occupationImage);
-        educationImage=findViewById(R.id.educationImage);
-        genderImage=findViewById(R.id.genderImage);
-        permissionImage=findViewById(R.id.permissionImage);
-        birthdayImage=findViewById(R.id.birthdayImage);
-        profileImage=findViewById(R.id.profileImage);
+//
+//        emailImage=findViewById(R.id.emailImage);
+//        occupationImage=findViewById(R.id.occupationImage);
+//        educationImage=findViewById(R.id.educationImage);
+//        genderImage=findViewById(R.id.genderImage);
+//        permissionImage=findViewById(R.id.permissionImage);
+//        birthdayImage=findViewById(R.id.birthdayImage);
+//        profileImage=findViewById(R.id.profileImage);
 
         database=FirebaseDatabase.getInstance();
         userRef=database.getReference("Users");
@@ -59,22 +57,22 @@ public class UserProfile extends AppCompatActivity {
 
                 for(DataSnapshot ds:dataSnapshot.getChildren()){
                     if(ds.child("username").getValue().equals(emailStr)){
-                        name.setText("Full name: "+ds.child("firstName").getValue(String.class)+" "+ds.child("lastName").getValue(String.class));
+                        name.setText(ds.child("firstName").getValue(String.class)+" "+ds.child("lastName").getValue(String.class));
 
-                        email.setText("Email: "+emailStr);
+                        email.setText(emailStr);
 
                         if(ds.child("occupation").exists()){
-                        occupation.setText("Occupation: "+ds.child("permission").getValue(String.class));}
+                        occupation.setText(ds.child("permission").getValue(String.class));}
                         else{occupation.setText("Occupation: ");}
 
                         if(ds.child("education").exists()){
-                            education.setText("Education"+ds.child("education").getValue(String.class));}
+                            education.setText(ds.child("education").getValue(String.class));}
                         else{education.setText("Education:");}
 
-                        permission.setText("Permission: "+ds.child("permission").getValue(String.class));
+                        permission.setText(ds.child("permission").getValue(String.class));
 
-                        birthday.setText("Birthday: "+ds.child("dateOfBirth").getValue(String.class));
-                        gender.setText("Gender: "+ds.child("gender").getValue(String.class));
+                        birthday.setText(ds.child("dateOfBirth").getValue(String.class));
+                        gender.setText(ds.child("gender").getValue(String.class));
                     }
                 }
             }
