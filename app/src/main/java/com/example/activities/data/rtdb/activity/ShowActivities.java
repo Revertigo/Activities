4ef3144ef123d23730bc95759825e1e5b6f00257
orh92 <orh92@walla.co.il>
2020-01-05 23:51:34 +0200

    Add filter that showing only the relevant Activities by time and date

--- app/src/main/java/com/example/activities/data/rtdb/activity/ShowActivities.java
+++ app/src/main/java/com/example/activities/data/rtdb/activity/ShowActivities.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,7 +15,11 @@ import com.example.activities.R;
 import com.example.activities.SearchActivity.SearchActivity;
 import com.example.activities.SearchActivity.SearchOrPost;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ShowActivities extends AppCompatActivity {
     private Button searchOrPost;
@@ -45,6 +50,49 @@ public class ShowActivities extends AppCompatActivity {
 
         final ArrayList<Activity> theActivities;
         theActivities = getIntent().getParcelableArrayListExtra("activitiesArray");
+        for (int i = 0; i < theActivities.size(); i++) {
+            String[] date = {theActivities.get(i).getDate().getDay(), theActivities.get(i).getDate().getMonth(), theActivities.get(i).getDate().getYear()};
+            String[] time = theActivities.get(i).getTime().split(":");
+            DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date dateobj = new Date();
+            String[] dateAndTime = df.format(dateobj).split(" ");
+
+            String[] dateStr = dateAndTime[0].split("/");
+            String[] timeStr = dateAndTime[1].split(":");
+            Log.wtf("dateStr=",dateStr[0]+"/"+dateStr[1]+"/"+dateStr[2]);
+            Log.wtf("timeStr=",timeStr[0]+":"+timeStr[1]);
+            //check if year passed
+            if (Integer.parseInt(date[2]) < Integer.parseInt(dateStr[2])) {
+                theActivities.remove(i);
+
+            } else {//year not passed so check month
+                if (date[2].equals(dateStr[2])) {//if year are equals check month
+                    if ((Integer.parseInt(date[1]) < Integer.parseInt(dateStr[1]))) {//if month passed remove activity
+                        theActivities.remove(i);
+                    } else {//month not passed so check days
+                        if (date[1].equals(dateStr[1])) {//if same month check days
+                            if (Integer.parseInt(date[0]) < Integer.parseInt(dateStr[0])) {//if day passed so remove
+                                theActivities.remove(i);
+                            } else {//day is fine check the time.
+                                if (date[0].equals(dateStr[0])) {//if we are in same day check time else all is fine
+                                    if (Integer.parseInt(time[0]) < Integer.parseInt(timeStr[0])) {//if hour is passed so remove else check min
+                                        theActivities.remove(i);
+                                    } else {
+                                        if (time[0].equals(timeStr[0])) {//if we are in same hour check min
+                                            if (Integer.parseInt(time[1]) < Integer.parseInt(timeStr[1])) {//if mid passed remove
+                                                theActivities.remove(i);
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         ListView activityListView = findViewById(R.id.listViewShowActivities);
         ArrayAdapter<Activity> adapter = new ArrayAdapter<Activity>(ShowActivities.this, android.R.layout.simple_list_item_1, theActivities);
         activityListView.setAdapter(adapter);
