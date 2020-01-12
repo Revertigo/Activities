9e61ab1fababe864d64e924d32db006e80f7bbdb
orh92 <orh92@walla.co.il>
2020-01-12 16:50:23 +0200

    Use PostUser/User interface

---
+++ app/src/main/java/com/example/activities/data/rtdb/activity/PostUserProfileEditable.java
@@ -0,0 +1,14 @@
+package com.example.activities.data.rtdb.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostUserProfileEditable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_user_profile_editable);
+    }
+}
